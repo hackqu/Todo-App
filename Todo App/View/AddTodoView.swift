5a79c7426c250066d9ed2a60547b5ea5cf7b1279
hackqu <tsaqifammar.jr@gmail.com>
2021-03-08 11:47:33 +0700

    Hari pertama

--- Todo App/View/AddTodoView.swift
+++ Todo App/View/AddTodoView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct AddTodoView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @State private var name: String = ""
+    @State private var priority: String = "Normal"
+    
+    let priorities = ["High", "Normal", "Low"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                Form{
+                    TextField("Todo", text: $name)
+                    
+                    Picker("Priority", selection: $priority){
+                        ForEach(priorities,id: \.self){
+                            Text($0)
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    
+                    Button(action: {
+                        print("Save a new todo item")
+                        
+                    }){
+                        Text("save")
+                    }
+                }
+                Spacer()
+            }
+            .navigationBarTitle("New Todo", displayMode: .inline)
+            .navigationBarItems(trailing:
+                                    Button(action: {
+                                        self.presentationMode.wrappedValue.dismiss()
+                                    }){
+                                        Image(systemName: "plus")
+                                    })
+        }
+        
     }
 }
 
