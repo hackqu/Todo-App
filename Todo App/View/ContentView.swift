9138134a28e9642c749e8824ce49c6a0e5469ea8
hackqu <tsaqifammar.jr@gmail.com>
2021-03-10 21:43:31 +0700

    Hari ke 5

--- Todo App/View/ContentView.swift
+++ Todo App/View/ContentView.swift
@@ -49,8 +49,27 @@ struct ContentView: View {
                     EmptyListView()
                 }
             }
+            .sheet(isPresented: $showingAddTodoView){
+                AddTodoView().environment(\.managedObjectContext, self.managedObjectContext)
+            }
+            .overlay(
+                ZStack{
+                    Button(action: {
+                        self.showingAddTodoView.toggle()
+                    }){
+                        Image(systemName: "plus.circle.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .background(Circle().fill(Color("ColorBase")))
+                            .frame(width: 48,height: 48, alignment: .center)
+                            
+                    }
+                }
+                .padding(.bottom, 15)
+                .padding(.trailing, 15)
+                , alignment: .bottomTrailing
             
-            
+            )
         }
         
     }
